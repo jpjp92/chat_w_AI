7575dc8993e748367961ad722662807985faae0d
Juan Park <111858577+jpjp92@users.noreply.github.com>
2025-04-15 22:21:13 +0900

    Update imports.py

--- config/imports.py
+++ config/imports.py
@@ -13,6 +13,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from googlesearch import search
 from g4f.client import Client
+from g4f.Provider import Blackbox
 from timezonefinder import TimezoneFinder
 import re
 import json
